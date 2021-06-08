595d978b31812dd1938236cf9af0c32e50ab2cf2
Caio Arrabal <caio.arrabal@e-thinkers.com.br>
2021-06-08 17:01:58 -0300

    Adicionado exemplo da funcao initialize.

--- JavascriptSamples/view/frontend/web/js/samples.js
+++ JavascriptSamples/view/frontend/web/js/samples.js
@@ -7,6 +7,13 @@ define([
             template: 'Caio_JavascriptSamples/content.html'
         },
 
+        initialize: function(){
+            this._super(); // Não se esqueça de chamar essa função!
+
+            console.log('%c        FUNÇÃO INITIALIZE', 'color: red; font-size: 20px')
+            console.log('Executada toda vez que esse arquivo JS for iniciado ou executado em outro arquivo JS!')
+        },
+
         myClickFunction: function(){
             alert('Heeey! Eu fui clicado!')
         },
